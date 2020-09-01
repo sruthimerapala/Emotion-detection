eaa0741489c011c48d6134ce3ee1514545fdfd3b
sruthimerapala <70164897+sruthimerapala@users.noreply.github.com>
2020-09-01 21:59:17 +0530

    Create JavaScript.js

---
+++ JavaScript.js
@@ -0,0 +1,2 @@
+const myHeading = document.querySelector('p');
+myHeading.textContent='JavaScript("JS" for short)is a dynamic computer programming language. It is lightweight and most commonly used as a part of web pages, whose implementations allow client-side script to interact with the user and make dynamic pages. It is an interpreted programming language with object-oriented capabilities.';
